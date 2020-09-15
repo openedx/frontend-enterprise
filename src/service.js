72877f690306e26cb7edc96a6e3fe2160d5fac13
Adam Stankiewicz <agstanki@gmail.com>
2020-09-15 07:56:42 -0400

    fix: only fetch user's preferred enterprise metadata

--- src/service.js
+++ src/service.js
@@ -1,4 +1,19 @@
-const fetchEnterpriseCustomers = (apiClient, lmsBaseUrl = process.env.LMS_BASE_URL) => apiClient.get(`${lmsBaseUrl}/enterprise/api/v1/enterprise-customer/`);
+import qs from 'querystring';
+
+const fetchEnterpriseCustomers = (
+  apiClient,
+  lmsBaseUrl = process.env.LMS_BASE_URL,
+  preferredEnterpriseUUID = null,
+) => {
+  let url = `${lmsBaseUrl}/enterprise/api/v1/enterprise-customer/`;
+  if (preferredEnterpriseUUID !== null) {
+    const queryParams = qs.stringify({
+      uuid: preferredEnterpriseUUID,
+    });
+    url += `?${queryParams}`;
+  }
+  return apiClient.get(url);
+};
 
 // eslint-disable-next-line import/prefer-default-export
 export { fetchEnterpriseCustomers };
