1de97b4f1d3b8f70dae5bfb477dd7986205fd7cd
binodpant <bpant@edx.org>
2020-11-12 11:01:07 -0500

    Start refactor, from utils folder and needed deps

---
+++ src/course-search/data/utils.js
@@ -0,0 +1,15 @@
+export const sortItemsByLabelAsc = items =>
+  items.sort((a, b) => a.label.localeCompare(b.label));
+
+export const updateRefinementsFromQueryParams = (refinements) => {
+  const refinementsWithJoinedLists = {};
+  Object.entries(refinements).forEach(([key, value]) => {
+    let newValue = value;
+    if (Array.isArray(value)) {
+      newValue = value.join(',');
+    }
+    refinementsWithJoinedLists[key] = newValue;
+  });
+
+  return refinementsWithJoinedLists;
+};
