10eb73d2a72d485e4a9cc57bcc6c17e59a20d67f
Adam Stankiewicz <agstanki@gmail.com>
2021-05-20 08:37:00 -0400

    fix: add temp export to test publish

--- packages/logistration/src/index.js
+++ packages/logistration/src/index.js
@@ -1,2 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
 export { default as LoginRedirect } from './LoginRedirect';
+
+// temporarily modify package to test/QA lerna publishing
+export const DELETE_ME = 'DELETE_ME';
