1c490a035810433c28ac622c6d07f77017ffb0f0
Muneeb Ur Rehman <muneeb.github@gmail.com>
2021-10-25 15:04:48 +0500

    fix: dropdown items not showing issue

--- packages/catalog-search/src/LearningTypeRadioFacet.jsx
+++ packages/catalog-search/src/LearningTypeRadioFacet.jsx
@@ -1,20 +1,19 @@
 import React, { useContext } from 'react';
 import classNames from 'classnames';
 import { Dropdown, Input } from '@edx/paragon';
-import { connectRefinementList } from 'react-instantsearch-dom';
 import { SearchContext } from './SearchContext';
 import {
   setRefinementAction,
 } from './data/actions';
 
-const LEARNING_TYPE_ANY = 'any';
 const LEARNING_TYPE_COURSE = 'course';
 const LEARNING_TYPE_PROGRAM = 'program';
 const LearningTypeRadioFacet = () => {
   const { refinements, dispatch } = useContext(SearchContext);
   // only bold the dropdown title if the learning type is Course or Program
-  const boldTitle = refinements.content_type && (refinements.content_type.includes(LEARNING_TYPE_COURSE)
-      || refinements.content_type.includes(LEARNING_TYPE_PROGRAM));
+  const typeCourseSelected = refinements['content_type'] && refinements['content_type'].includes(LEARNING_TYPE_COURSE)
+  const typeProgramSelected = refinements['content_type'] && refinements['content_type'].includes(LEARNING_TYPE_PROGRAM)
+  const boldTitle = typeCourseSelected || typeProgramSelected
 
   const handleInputOnChange = (type) => {
     if (type === '') {
@@ -41,30 +40,30 @@ const LearningTypeRadioFacet = () => {
               className="facet-item position-relative mr-2 mb-2"
               onChange={() => handleInputOnChange('')}
             />
-            <span className={classNames('facet-item-label', 'is-refined')}>
-              {LEARNING_TYPE_ANY}
+            <span className={classNames('facet-item-label', {})}>
+              Any
             </span>
           </Dropdown.Item>
           <Dropdown.Item as="label" className="mb-0 py-3 d-flex align-items-center">
             <Input
               type="radio"
-              checked={refinements.content_type && refinements.content_type.includes(LEARNING_TYPE_COURSE)}
+              checked={typeCourseSelected}
               className="facet-item position-relative mr-2 mb-2"
               onChange={() => handleInputOnChange(LEARNING_TYPE_COURSE)}
             />
-            <span className={classNames('facet-item-label', 'is-refined')}>
-              {LEARNING_TYPE_COURSE}
+            <span className={classNames('facet-item-label', {'is-refined':typeCourseSelected})}>
+              Courses
             </span>
           </Dropdown.Item>
           <Dropdown.Item as="label" className="mb-0 py-3 d-flex align-items-center">
             <Input
               type="radio"
-              checked={refinements.content_type && refinements.content_type.includes(LEARNING_TYPE_PROGRAM)}
+              checked={typeProgramSelected}
               className="facet-item position-relative mr-2 mb-2"
               onChange={() => handleInputOnChange(LEARNING_TYPE_PROGRAM)}
             />
-            <span className={classNames('facet-item-label', 'is-refined')}>
-              {LEARNING_TYPE_PROGRAM}
+            <span className={classNames('facet-item-label', {'is-refined':typeProgramSelected})}>
+              Programs
             </span>
           </Dropdown.Item>
         </Dropdown.Menu>
@@ -73,4 +72,4 @@ const LearningTypeRadioFacet = () => {
   );
 };
 
-export default connectRefinementList(LearningTypeRadioFacet);
+export default LearningTypeRadioFacet;
