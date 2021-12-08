6f9d407783eba7000120086c924782cf9e8152a9
Adam Stankiewicz <agstanki@gmail.com>
2021-12-08 07:58:03 -0500

    docs: add jsdoc comments

--- packages/logistration/src/utils.js
+++ packages/logistration/src/utils.js
@@ -1,3 +1,12 @@
+/**
+ * Given either an `enterpriseSlug` or an `enterpriseCustomerInviteKey`, returns a
+ * URL to the enterprise proxy login page in the LMS. The proxy-login page appropriately
+ * redirects enterprise users to the enterprise-specific logistration flow.
+ *
+ * @param {string} enterpriseSlug Slug of an enterprise customer
+ * @param {string} enterpriseCustomerInviteKey UUID of an EnterpriseCustomerInviteKey
+ * @returns URL of the enterprise proxy login page in the LMS.
+ */
 // eslint-disable-next-line import/prefer-default-export
 export const getProxyLoginUrl = (enterpriseSlug, enterpriseCustomerInviteKey) => {
   const queryParams = new URLSearchParams();
