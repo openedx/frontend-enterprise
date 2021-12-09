9b8a200633d5fc665ed9faf79d10f43cb7a9386a
Adam Stankiewicz <agstanki@gmail.com>
2021-12-09 09:55:32 -0500

    feat: support enterprise customer invite key in LoginRedirect (#192)
    * feat: support enterprise customer invite key in LoginRedirect
    
    * fix: remove query-string package
    
    * fix: lint
    
    * docs: add jsdoc comments

--- packages/logistration/src/utils.js
+++ packages/logistration/src/utils.js
@@ -1,11 +1,22 @@
-import qs from 'query-string';
-
+/**
+ * Given either an `enterpriseSlug` or an `enterpriseCustomerInviteKey`, returns a
+ * URL to the enterprise proxy login page in the LMS. The proxy-login page appropriately
+ * redirects enterprise users to the enterprise-specific logistration flow.
+ *
+ * @param {string} enterpriseSlug Slug of an enterprise customer
+ * @param {string} enterpriseCustomerInviteKey UUID of an EnterpriseCustomerInviteKey
+ * @returns URL of the enterprise proxy login page in the LMS.
+ */
 // eslint-disable-next-line import/prefer-default-export
-export const getProxyLoginUrl = (enterpriseSlug) => {
-  const options = {
-    enterprise_slug: enterpriseSlug,
-    next: global.location,
-  };
-  const proxyLoginUrl = `${process.env.LMS_BASE_URL}/enterprise/proxy-login/?${qs.stringify(options)}`;
+export const getProxyLoginUrl = (enterpriseSlug, enterpriseCustomerInviteKey) => {
+  const queryParams = new URLSearchParams();
+  queryParams.append('next', global.location);
+  if (enterpriseSlug) {
+    queryParams.append('enterprise_slug', enterpriseSlug);
+  }
+  if (enterpriseCustomerInviteKey) {
+    queryParams.append('enterprise_customer_invite_key', enterpriseCustomerInviteKey);
+  }
+  const proxyLoginUrl = `${process.env.LMS_BASE_URL}/enterprise/proxy-login/?${queryParams.toString()}`;
   return proxyLoginUrl;
 };
