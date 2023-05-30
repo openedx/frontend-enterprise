c604834d2efcfeba5d692e0f8dc7bb1681e72262
Katrina Nguyen <71999631+katrinan029@users.noreply.github.com>
2023-05-30 13:44:53 -0700

    fix: missing space in search (#333)

---
+++ packages/catalog-search/src/tests/SearchSuggestionItem.test.jsx
@@ -0,0 +1,115 @@
+import { renderWithRouter } from '@edx/frontend-enterprise-utils';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import SearchSuggestionItem from '../SearchSuggestionItem';
+
+describe('<SeachSuggestionItem />', () => {
+  test('renders course data', () => {
+    const mockData = {
+      course: {
+        url: '/test-enterprise/course/edX+courseX',
+        suggestionItemHandler: jest.fn(),
+        disableSuggestionRedirect: true,
+        hit: {
+          content_type: 'course',
+          key: 'edX+courseX',
+          title: 'test-course',
+          _highlightResult: { title: { value: '<em>course</em> <em>catalog</em>' } },
+        },
+      },
+    };
+
+    renderWithRouter(<SearchSuggestionItem
+      url={mockData.course.url}
+      suggestionItemHandler={mockData.course.suggestionItemHandler}
+      hit={mockData.course.hit}
+      disableSuggestionRedirect={mockData.course.disableSuggestionRedirect}
+    />);
+    expect(screen.getByRole('link', { name: 'course catalog edX' })).not.toBeNull();
+    expect(screen.getByText('course')).not.toBeNull();
+    expect(screen.getByText('catalog')).not.toBeNull();
+    expect(screen.getByText('edX')).not.toBeNull();
+  });
+
+  test('renders program data', () => {
+    const mockData = {
+      program: {
+        url: '/test-enterprise/program/456',
+        suggestionItemHandler: jest.fn(),
+        disableSuggestionRedirect: true,
+        hit: {
+          content_type: 'program',
+          program_type: 'Professional Program',
+          key: 'edX+programX',
+          title: 'test-program',
+          _highlightResult: { title: { value: '<em>program</em> <em>catalog</em>' } },
+        },
+      },
+    };
+
+    renderWithRouter(<SearchSuggestionItem
+      url={mockData.program.url}
+      suggestionItemHandler={mockData.program.suggestionItemHandler}
+      hit={mockData.program.hit}
+      disableSuggestionRedirect={mockData.program.disableSuggestionRedirect}
+    />);
+    expect(screen.getByRole('link', { name: 'program catalog edX Professional Program' })).not.toBeNull();
+    expect(screen.getByText('program')).not.toBeNull();
+    expect(screen.getByText('catalog')).not.toBeNull();
+    expect(screen.getByText('edX')).not.toBeNull();
+    expect(screen.getByText('Professional Program')).not.toBeNull();
+  });
+
+  test('redirects on click if disableSuggestionRedirect is false', () => {
+    const mockData = {
+      program: {
+        url: '/test-enterprise/program/456',
+        suggestionItemHandler: jest.fn(),
+        disableSuggestionRedirect: false,
+        hit: {
+          content_type: 'program',
+          program_type: 'Professional Program',
+          key: 'edX+programX',
+          title: 'test-program',
+          _highlightResult: { title: { value: '<em>program</em> <em>catalog</em>' } },
+        },
+      },
+    };
+
+    const { container, history } = renderWithRouter(<SearchSuggestionItem
+      url={mockData.program.url}
+      suggestionItemHandler={mockData.program.suggestionItemHandler}
+      hit={mockData.program.hit}
+      disableSuggestionRedirect={mockData.program.disableSuggestionRedirect}
+    />);
+    userEvent.click(container.getElementsByClassName('suggestion-item')[0]);
+    expect(history.location.pathname).toBe(mockData.program.url);
+  });
+
+  test('fires callback on click if disableSuggestionRedirect is true', () => {
+    const mockData = {
+      program: {
+        url: '/test-enterprise/program/456',
+        suggestionItemHandler: jest.fn(),
+        disableSuggestionRedirect: true,
+        hit: {
+          content_type: 'program',
+          program_type: 'Professional Program',
+          key: 'edX+programX',
+          title: 'test-program',
+          _highlightResult: { title: { value: '<em>program</em> <em>catalog</em>' } },
+        },
+      },
+    };
+
+    const { container } = renderWithRouter(<SearchSuggestionItem
+      url={mockData.program.url}
+      suggestionItemHandler={mockData.program.suggestionItemHandler}
+      hit={mockData.program.hit}
+      disableSuggestionRedirect={mockData.program.disableSuggestionRedirect}
+    />);
+    userEvent.click(container.getElementsByClassName('suggestion-item')[0]);
+    expect(mockData.program.suggestionItemHandler).toHaveBeenCalledWith(mockData.program.hit);
+  });
+});
